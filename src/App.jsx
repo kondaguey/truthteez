2948770af7ba4b2f6ed7f8a43a8b46d14758a904
Daniel Lewis <dlew@macbook-air.myfiosgateway.com>
2022-07-06 12:08:21 -0400

    add teaser

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@
 function App() {
   return (
 
-  <div>
-     <h1>Hello 🌏! If I'm pink, then you're good to go!</h1>
+  <div className="main">
+     <h1 className="main__header">🎉 Grand Opening: August, 2022 🎉</h1>
+     <h2 className="main__subheader">the only appearal that tells the Truth</h2>
+     <img className="main__logo" src="img/logoBlack.png" alt="Truth Teez Logo" />
   </div>
 
   );
