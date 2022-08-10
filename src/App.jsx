609b085a8b43c6d06ab2f6a18f8ac7b5a928df81
Daniel Lewis <dlew@Daniels-MacBook-Air.local>
2022-08-10 14:45:52 -0400

    add temporary store link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
+
 function App() {
   return (
 
   <div className="main">
-     <h1 className="main__header">🎉 Grand Opening: August, 2022 🎉</h1>
+     <h1 className="main__header">🎉 Official Webstore Grand Opening: Early September, 2022 🎉</h1>
+     <h2 className="main__redirect"><a target="_blank" href="https://truthteez-3.creator-spring.com/" >Current Store Here</a></h2>
      <h2 className="main__subheader">the only appearal that tells the Truth</h2>
      <img className="main__logo" src="img/logoBlack.png" alt="Truth Teez Logo" />
+     
   </div>
 
   );
